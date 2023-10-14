73d6f568b0dd281203420985fb51ed7a8766bf2c
Dayowe <dayowe@tutamail.com>
2023-10-14 16:30:41 +0200

    Initial commit

---
+++ chiller_pump.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+self.logger.info("Ensuring Chiller pump is on.")
+
+self.loop_count += 1  # Counts how many times the run code has been executed
+
+chiller_pump_state = self.condition("081e57dd")
+self.logger.debug(f"Chiller Pump is {chiller_pump_state}")
+
+if chiller_pump_state == "on":
+    self.logger.debug(f"The Chiller Pump is {chiller_pump_state}, as it should be.")
+    self.message += f"The Chiller Pump is {chiller_pump_state}, as it should be.\n"
+
+elif chiller_pump_state == "off":
+    self.logger.debug(f"The Chiller Pump is {chiller_pump_state}, but should be on. Activating pump.")
+    self.message += f"Chiller Pump is {chiller_pump_state}, but should be on.\nActivating pump."
+    self.run_action("867d958c")
