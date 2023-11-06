626f4ce62cf66916b143a5c0c66437b3f805a835
Dayowe <dayowe@tutamail.com>
2023-11-06 19:07:10 +0100

    Minor changes

--- ensure_climate.py
+++ ensure_climate.py
@@ -2,6 +2,10 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 
 # Work in progress
+# Seedling stage, ambient temp ~ 24.5C
+# 77, 79, 83
+#After a few days I changed to
+# 74, 76, 80
 
 # check if humidifier is running or not
 GPIO.setmode(GPIO.BCM)
@@ -12,17 +16,17 @@ self.logger.debug("Ensuring climate is in check")
 
 self.loop_count += 1  # Counts how many times the run code has been executed
 
-early_veg = True
-late_veg_early_flower = False
-mid_late_flower = False
+# early_veg = True
+# late_veg_early_flower = False
+# mid_late_flower = False
 
-seedling_humidity_target_range = range(70,75)
-seedling_temperature_target_range = range(24,28)
+# seedling_humidity_target_range = range(70,75)
+# seedling_temperature_target_range = range(24,28)
 
 # range and float don't work, so target values * 1000
-early_veg_vpd_target_range = range(400,800)
-late_veg_early_flower_vpd_target_range = range(800,1200)
-mid_late_flower_vpd_target_range = range(1200,1600)
+# early_veg_vpd_target_range = range(400,800)
+# late_veg_early_flower_vpd_target_range = range(800,1200)
+# mid_late_flower_vpd_target_range = range(1200,1600)
 
 if vpd is not None:
     vpd = round(self.condition("5f792188"),2)
@@ -38,14 +42,14 @@ if humidity is not None:
     self.logger.debug(f"Humidity is {humidity}")
     self.run_action("94a73d30", value={"topic": "mycodo/measurements/humidity/ambient", "payload": humidity})
     if humidity not in seedling_humidity_target_range:
-        if humidity > 83:
+        if humidity > 80:
             self.message += "Humidity is way too high!\n"
             self.logger.debug("Humidity is way too high!")
-        if humidity > 78 and humidifier_state == 1:
+        if humidity > 76 and humidifier_state == 1:
             self.message += f"Humidity is too High! Humidity is {humidity}\n"
             self.logger.debug(f"Humidity is too High! Humidity is {humidity}. Deactivating humidifier.")
             self.run_action("d0792870")
-        elif humidity < 70 and humidifier_state == 0:
+        elif humidity < 74 and humidifier_state == 0:
             self.message += f"Humidity is too Low! Humidity is {humidity}\n"
             self.logger.debug(f"Humidity is too Low! Humidity is {humidity}. Activating humidifier.")
             self.run_action("20d5d009")
