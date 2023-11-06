4c4b3fbd867407de3b81995773bb8733ef973ca7
Dayowe <dayowe@tutamail.com>
2023-11-06 19:27:41 +0100

    Fixes

--- ensure_climate.py
+++ ensure_climate.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import RPi.GPIO as GPIO
+from RPi import GPIO
 
 # Work in progress
 # Seedling stage, ambient temp ~ 24.5C
@@ -20,7 +20,7 @@ self.loop_count += 1  # Counts how many times the run code has been executed
 # late_veg_early_flower = False
 # mid_late_flower = False
 
-# seedling_humidity_target_range = range(70,75)
+seedling_humidity_target_range = range(70,75)
 # seedling_temperature_target_range = range(24,28)
 
 # range and float don't work, so target values * 1000
@@ -28,8 +28,34 @@ self.loop_count += 1  # Counts how many times the run code has been executed
 # late_veg_early_flower_vpd_target_range = range(800,1200)
 # mid_late_flower_vpd_target_range = range(1200,1600)
 
-if vpd is not None:
+humidity = None
+temperature = None
+vpd = None
+
+# Handle humidity measurement
+try:
+    humidity = round(self.condition("83498920"),2)
+    self.run_action("94a73d30", value={"topic": "mycodo/measurements/humidity/ambient", "payload": humidity})
+except TypeError:
+    self.message += "Could not get a reading for humidity!\n"
+    self.logger.error("Could not get a reading for humidity!")
+
+# Handle temperature measurement
+try:
+    temperature = round(self.condition("0040d310"),2)
+    self.run_action("94a73d30", value={"topic": "mycodo/measurements/climate/temperature_garden", "payload": temperature})
+except TypeError:
+    self.message += "Could not get a reading for temperature!\n"
+    self.logger.error("Could not get a reading for temperature!")
+
+# Handle VPD measurement
+try:
     vpd = round(self.condition("5f792188"),2)
+except TypeError:
+    self.message += "Could not get a reading for VPD!\n"
+    self.logger.error("Could not get a reading for VPD!")
+
+if vpd is not None:
     self.logger.debug(f"VPD is {vpd}")
     self.run_action("94a73d30", value={"topic": "mycodo/measurements/climate/vpd", "payload": vpd})
 else:
@@ -38,7 +64,6 @@ else:
 
 # for now go by humidity instead of VPD
 if humidity is not None:
-    humidity = round(self.condition("83498920"),2)
     self.logger.debug(f"Humidity is {humidity}")
     self.run_action("94a73d30", value={"topic": "mycodo/measurements/humidity/ambient", "payload": humidity})
     if humidity not in seedling_humidity_target_range:
@@ -58,9 +83,8 @@ else:
     self.logger.error("Could not get a reading for humidity!")
 
 if temperature is not None:
-        temperature = round(self.condition("0040d310"),2)
-        self.logger.debug(f"Temperature is {temperature}")
-        self.run_action("94a73d30", value={"topic": "mycodo/measurements/climate/temperature_garden", "payload": temperature})
+    self.logger.debug(f"Temperature is {temperature}")
+    self.run_action("94a73d30", value={"topic": "mycodo/measurements/climate/temperature_garden", "payload": temperature})
 else:
     self.message += "Could not get a reading for temperature!\n"
     self.logger.error("Could not get a reading for temperature!")
